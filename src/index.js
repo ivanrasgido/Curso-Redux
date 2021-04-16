01963526eea83814a8e4a6cd8cd3b1c1a812d622
ivanrasgido <ivanrasgido19@gmail.com>
2021-04-16 20:40:32 -0300

    Unidad 3 de redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { bugAdded, bugResolved } from "./actions";
+import store from "./store";
+
+// state = reducer(state , action) ;
+//notify the subscribers
+
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
