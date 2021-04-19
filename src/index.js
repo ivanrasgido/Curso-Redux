8f0ccee2c5a77b9aac8e1d603822784b4a932e57
ivanrasgido <ivanrasgido19@gmail.com>
2021-04-19 10:10:12 -0300

    Unidad 8

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import configureStore from "./store/configureStore";
-import {
-  bugAdded,
-  bugResolved,
-  bugAssignedToUser,
-  getUnresolvedBugs,
-  getBugsByUser,
-} from "./store/bugs";
-import { projectAdded } from "./store/proyects";
-import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("Store changed!");
+store.dispatch({
+  type: "error",
+  payload: { message: "An error occurred." },
 });
-
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs);
